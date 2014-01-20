b621c02b7f15262ca19b98f6b4408bed9224b6e9
Oron Peled <oron.peled@xorcom.com>
2014-01-20 20:10:56 +0200

    Change span-type.conf generation policy
    * Add '--line-mode=<E1|T1|J1>' option to dahdi_span_types:
      - Will first generate a wildcard entry (* *:<type>)
      - All later entries will be commented-out.
      - Manually uncommenting any of them will create an "override"
        (e.g: mixed system with all spans T1, but some E1)
    
    * Now dahdi_genconf does not generate span-types.conf by default:
      - Added '--line-mode=<E1|T1|J1>' option to trigger generation
      - This parameter will be passed to "dahdi_span_types dumpconfig"
      - Also explicit specification of 'spantypes' as an argument will
        trigger a similar generation (with a default of 'E1').
      - The line-mode may also be passed to the generator directly
        with identical results. Example:
              dahdi_genconf spantypes=line-mode=T1
    
    Signed-off-by: Tzafrir Cohen <tzafrir.cohen@xorcom.com>
    Acked-By: Russ Meyerriecks <rmeyerriecks@digium.com>

--- xpp/perl_modules/Dahdi/Config/Gen/Spantypes.pm
+++ xpp/perl_modules/Dahdi/Config/Gen/Spantypes.pm
@@ -29,13 +29,17 @@ sub generate($$$) {
 	system "which dahdi_span_types > /dev/null 2>&1";
 	return if $?;
 
+	my $line_mode = $genopts->{'line-mode'};
+	$line_mode = 'E1' unless defined $line_mode;
+	$line_mode =~ /^[ETJ]1$/ or die "Bad line-mode='$line_mode'\n";
 	warn "Empty configuration -- no spans\n" unless @spans;
 	rename "$file", "$file.bak"
 		or $! == 2	# ENOENT (No dependency on Errno.pm)
 		or die "Failed to backup old config: $!\n";
 	#$gconfig->dump;
-	print "Generating $file\n" if $genopts->{verbose};
-	my $cmd = "dahdi_span_types dumpconfig > $file";
+	printf("Generating $file (with default line-mode %s)\n", $line_mode)
+		if $genopts->{verbose};
+	my $cmd = "dahdi_span_types --line-mode=$line_mode dumpconfig > $file";
 	system $cmd;
 	die "Command failed (status=$?): '$cmd'" if $?;
 }
@@ -61,3 +65,17 @@ Generate the F</etc/dahdi/span-types.conf>.
 This is the configuration for dahdi_span_types.
 
 Its location may be overriden via the environment variable F<SPAN_TYPES_CONF_FILE>.
+
+You would normally run:
+
+  dahdi_genconf --line-mode=<line_mode>
+
+which is a short for:
+
+  dahdi_genconf spantypes=line-mode=<line_mode>
+
+This is done by running:
+  dahdi_span_types dumpconfig --line-mode=line_mode>
+
+where I<line_mode> is the module parameter, and defaults to B<E1> if not
+given (running C<dahdi_genconf spantypes>).
