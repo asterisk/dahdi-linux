bc0c04fb1348e64c36c3a7557a97cdafb28300f3
Shaun Ruffell <sruffell@digium.com>
2014-03-17 11:54:27 -0500

    wcaxx: Update firmware for A8B/A4B to 1d0019/b0019.
    These firmware images can handle system conditions that would previously result
    in transmitted audio being intermittently silenced and then unsilenced.
    
    Internal-Issue-ID: DAHDI-1087
    Signed-off-by: Shaun Ruffell <sruffell@digium.com>

--- drivers/dahdi/wcaxx-base.c
+++ drivers/dahdi/wcaxx-base.c
@@ -3886,9 +3886,9 @@ static int wcaxx_check_firmware(struct wcaxx *wc)
 	u32 firmware_version;
 	const bool force_firmware = false;
 	const unsigned int A4A_VERSION = 0x0a0017;
-	const unsigned int A4B_VERSION = 0x0b0017;
+	const unsigned int A4B_VERSION = 0x0b0019;
 	const unsigned int A8A_VERSION = 0x1d0017;
-	const unsigned int A8B_VERSION = 0x1d0017;
+	const unsigned int A8B_VERSION = 0x1d0019;
 
 	if (wc->desc == &device_a8a) {
 		firmware_version = A8A_VERSION;
