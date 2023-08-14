8cdf93654473cca933fb4d867d499daa3848958e
duncanpatterson <42005699+duncanpatterson@users.noreply.github.com>
2023-08-14 16:09:14 +0100

    GitHub ci (#24)
    * Initial Github CI against linux kernels
    
    * Fixed path for linux-next source
    
    * Added perl-modules to Dockerfile
    
    * Removed some old CI files and tidied things up
    
    * Added CI for linux stable kernel
    
    * Schedule compile checks every Monday morning at 02:00

---
+++ ci/build-dahdi.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+cd /linux
+echo "### Cleaning Kernel tree"
+echo "  # rm .config"
+rm .config
+
+echo "  # make clean"
+make clean
+echo "### Get kernel version"
+make kernelversion
+echo "### Create defconfig and prepare for module building"
+echo "  # make x86_64_defconfig"
+make x86_64_defconfig
+echo "  # make modules_prepare"
+make modules_prepare
+
+echo "### Building DAHDI modules and installing"
+cd /src
+echo "  # make clean"
+make clean
+echo "  # make install KSRC=/linux"
+make install KSRC=/linux
+
