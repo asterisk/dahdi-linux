397e29fe3f0b42fde7ec72a7c2ddcb9ab5af95f6
Kenan Kocaerkek <kenankocaerkek@gmail.com>
2024-02-21 15:03:38 +0000

    fix compilation issue for distributions other than RHEL

--- include/dahdi/kernel.h
+++ include/dahdi/kernel.h
@@ -65,7 +65,7 @@
 /* Linux kernel 5.16 and greater has removed user-space headers from the kernel include path */
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 16, 0)
 #include <asm/types.h>
-#elif defined(RHEL_RELEASE_CODE) && LINUX_VERSION_CODE >= KERNEL_VERSION(5,14,0) && \
+#elif defined(RHEL_RELEASE_CODE) && defined(RHEL_RELEASE_VERSION) && LINUX_VERSION_CODE >= KERNEL_VERSION(5,14,0) && \
               RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9,1)
 #include <asm/types.h>
 #else
@@ -1562,10 +1562,12 @@ static inline void *PDE_DATA(const struct inode *inode)
 #endif
 #endif
 
-#if defined(RHEL_RELEASE_CODE) && LINUX_VERSION_CODE >= KERNEL_VERSION(5,14,0) && \
+#if defined(RHEL_RELEASE_CODE) && defined(RHEL_RELEASE_VERSION)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,14,0) && \
               RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9,1)
 #define netif_napi_add netif_napi_add_weight
 #define PDE_DATA(i)     pde_data(i)
+#endif
 #endif
 
 #ifndef TIMER_DATA_TYPE
